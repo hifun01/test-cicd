6cb4c4113a34efa7d2291b7da717d2ff6ddadb6a
hifun01 <hifun01@outlook.com>
2025-07-11 01:20:14 +0800

    test cicd

---
+++ src/main/java/com/example/cicdtest/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.cicdtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author hifun01@outlook.com
+ * @Date 2025/7/11 00:54
+ * @Description:
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
