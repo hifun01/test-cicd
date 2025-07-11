f5340c23e4f4e7207c9479f318c9f86071a81834
hifun01 <hifun01@outlook.com>
2025-07-12 01:25:28 +0800

    test cicd

--- src/main/java/com/example/cicdtest/controller/HelloController.java
+++ src/main/java/com/example/cicdtest/controller/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello CI-CD";
+        return "Hello CI/CD";
     }
 }
